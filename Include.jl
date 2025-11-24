4de624a1875071bd588b6fb1a2b5ad9235472c0c
John McGowan <jmcgowan5g@gmail.com>
2025-11-23 21:34:46 -0500

    Submission

--- Include.jl
+++ Include.jl
@@ -6,10 +6,17 @@ const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
 
 # make sure all is up to date -
 using Pkg
-if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+# Activate the project in this folder so `using` pulls from the right env
+Pkg.activate(_ROOT)
+
+# If the manifest is missing, pull the finance package into the env before
+# instantiating to get all dependencies in place
+if isfile(joinpath(_ROOT, "Manifest.toml")) == false
     Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
-    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
+
+# Ensure deps are installed locally (safe to run even when already instantiated)
+Pkg.instantiate()
 
 # load external packages -
 using VLQuantitativeFinancePackage
